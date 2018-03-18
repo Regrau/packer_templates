d60eca47b25541b7ef12e1a2a36da501a18415c9
Georg Grauberger <Georg.Grauberger@gmail.com>
2018-03-18 23:41:58 +0100

    Remove Password authentication from image

--- scripts/sanitize.sh
+++ scripts/sanitize.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # This script will change the password of the root user
 # and remove any password authentication from the image.
-# It also will bootstrap for ansible so we can execute
-# our playbooks locally.
 
-# Update all packages
-zypper update --no-recommends \
-    && zypper in ansible
+
+# Add no password option to SSHD Config file
+echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config
+sudo sed -i 's/UsePAM yes/UsePAM no/g'
+
+# No root login via SSH
+echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config
+
+# Allow salt user to login via ssh
+mkdir /home/salt/.ssh
+cp /tmp/authorized_keys /home/salt/.ssh/authorized_keys
+chown -R salt:users /home
+
+echo "salt ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
